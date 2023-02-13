fce9f67a1c5e3ee2f389c15cea49dc445ae802ac
LesGrooper <eqimulia@ymail.com>
2023-02-13 23:58:38 +0700

    adding some features on front end

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -3,21 +3,9 @@ const BookController = require("../controllers/BookController");
 const multer = require("../middleware/multer");
 
 bookRoute.get("/", BookController.getData);
+bookRoute.get("/status", BookController.getStatusData);
 bookRoute.get("/book/:id", BookController.getDataBy);
-bookRoute.post(
-  "/",
-  multer.upload.single("image"),
-  (req, resp) => {
-    let imageUrl =
-      req.protocol +
-      "://" +
-      req.get("host") +
-      "/img/uploads/" +
-      req.file.filename;
-    resp.json({ status: "success", image: imageUrl });
-  },
-  BookController.postData
-);
+bookRoute.post("/", multer.upload.single("image"), BookController.postData);
 bookRoute.put("/update/:id", BookController.putData);
 bookRoute.delete("/delete/:id", BookController.deleteData);
 
